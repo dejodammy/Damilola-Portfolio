31a2d7d208006cf2b1e80afd33769c1cc62d9967
dejodammy <95016055+dejodammy@users.noreply.github.com>
2024-06-20 19:19:02 +0100

    new mobile change

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,34 +2,23 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+
 const Tech = () => {
   const isMobile = window.matchMedia("(max-width: 500px)").matches;
-  const Map = (isMobile ,technologies)=>{
-    if(isMobile){
-      return(
-    technologies.slice(0,4).map((technology)=>(
-    <div className='w-28 h-28' key={technology.name}>
-    <BallCanvas icon={technology.icon}/>
-    <p className="flex justify-center text-white font-bold">{technology.name}</p>
-    </div>
-    )))
-    }
-    else{return(
-      technologies.map((technology)=>(
-        <div className='w-28 h-28' key={technology.name}>
-        <BallCanvas icon={technology.icon}/>
-        <p className="flex justify-center text-white font-bold">{technology.name}</p>
-        </div>
-      )))
-    }
-  }
+
   return (
     <>
-    <h1 className='font-bold flex justify-center text-lg mb-5 text-[#915EFF]'>My Tech Stack</h1>
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {Map(isMobile ,technologies)}
-    </div>
+      <h1 className='font-bold flex justify-center text-lg mb-5 text-[#915EFF]'>My Tech Stack</h1>
+      <div className={`flex flex-wrap justify-center gap-10 ${isMobile ? 'mobile-grid' : ''}`}>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+            <p className="flex justify-center text-white font-bold">{technology.name}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
+
 export default SectionWrapper(Tech, "");
