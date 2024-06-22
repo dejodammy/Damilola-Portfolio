42a481e254045955e11be1724a2daabaed6b6b30
dejodammy <95016055+dejodammy@users.noreply.github.com>
2024-06-22 22:20:20 +0100

    MOBILE AGAIN

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,25 +4,17 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 const Tech = () => {
   const isMobile = window.matchMedia("(max-width: 500px)").matches;
-  const Map = (isMobile ,technologies)=>{
-    if(isMobile){
-  return (
-    technologies.map((technology)=>(
-    <div className='w-28 h-28' key={technology.name}>
-    <BallCanvas icon={technology.icon}/>
-    <p className="flex justify-center text-white font-bold">{technology.name}</p>
-    </div>
-    )))
-    }
-    else{return(
-      technologies.map((technology)=>(
-          <div className='w-28 h-28' key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-            <p className="flex justify-center text-white font-bold">{technology.name}</p>
-          </div>
-      )))
-    }
-  }
+  const Map = (isMobile, technologies) => {
+    return technologies.map((technology) => (
+      <div className="w-28 h-28" key={technology.name}>
+        <BallCanvas icon={technology.icon} />
+        <p className="flex justify-center text-white font-bold">
+          {technology.name}
+        </p>
+      </div>
+    ));
+  };
+  
   return (
     <>
     <h1 className='font-bold flex justify-center text-lg mb-5 text-[#915EFF]'>My Tech Stack</h1>
